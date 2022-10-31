601ea0679872dda08a206c9ee2cf91ded573b387
Sergey Nedobezhkin <zelenka1633@gmail.com>
2022-10-31 22:54:09 +0400

    Устранил замечания lesson19.6

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+const div = document.createElement('div');
 let idInterval = 0;
+
 const timer = () => {
 	const date = new Date();
 	const newDate = new Date(new Date().getFullYear() + 1, 0, 1);
+
 	function greeting() {
 		const hours = date.getHours();
 
@@ -16,36 +19,38 @@ const timer = () => {
 		}
 	}
 
-	function weekDay(date) {
-		const dayOfTheWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-		return dayOfTheWeek[date.getDay()];
+	function weekDay() {
+		return new Date().toLocaleDateString('ru-RU', { weekday: 'long' })
+			.replaceAll(/([а-яё])([а-яё]*)/gi, (word, first, rest) => (`${first.toUpperCase()}${rest.toLowerCase()}`));
 	}
 
-	function timeDay() {
+	const timeDay = () => {
 		const timeOfDay = new Date().toLocaleTimeString('en-Us');
 		return `${timeOfDay}`;
-	}
-
-	const days = (number) => {
-		const deys = [' день', ' дня', ' дней'];
-		const deyOne = number % 100,
-			deyTwo = number % 10;
-		return deyOne > 4 && deyOne < 21 ? number + deys[2] :
-			deyTwo === 1 ? number + deys[0] :
-				deyTwo > 1 && deyTwo < 5 ? number + deys[1] :
-					number + deys[2];
 	};
 
-	return `${greeting()} Сегодня: ${weekDay(date)}
+	const days = (num, words) => {
+		let num10 = num % 10;
+		let num100 = num % 100;
+		switch (true) {
+			case num100 >= 10 && num100 <= 20: return `${num} ${words[2]}`;
+			case num10 === 1: return `${num} ${words[0]}`;
+			case num && num10 < 5: return `${num} ${words[1]}`;
+			default: return `${num} ${words[2]}`;
+		}
+
+	};
+
+	return `${greeting()} Сегодня: ${weekDay()}
   Текущее время: ${timeDay()}
-  До нового года осталось ${days(Math.ceil((newDate.getTime() - date.getTime()) / 1000 / 60 / 60 / 24))}`;
+  До нового года осталось ${days(Math.ceil((newDate.getTime() - date.getTime()) / 1000 / 60 / 60 / 24),
+		[' день', ' дня', ' дней'])}`;
 
 };
-timer();
+div.innerText = timer();
 idInterval = setInterval(() => {
-	div.innerText = timer();
+	div.innerText = timer();	
 }, 1000);
 
-const div = document.createElement('div');
 div.style.cssText = 'text-align: center;font-size: 30px;margin-top: 10%;';
 document.body.append(div);
